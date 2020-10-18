65a6a57dbfb0292d425e47d886a5dbe40ffd43c4
Ruben Paz <rubenpazchuspe@outlook.com>
2020-10-18 10:28:32 -0500

    add data time pricker to the form

--- contact.js
+++ contact.js
@@ -4,7 +4,7 @@ $(function () {
     // validator files are included in the download package
     // otherwise download from http://1000hz.github.io/bootstrap-validator
 
-    $('#contact-form').validator();
+    //$('#contact-form').validator();
 
 
     // when the form is submitted
--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
-
-$('#datepicker').datepicker({
-    uiLibrary: 'bootstrap4',
-    locale: 'es-es',
-    format: 'dd/mm/yyyy',
-  });
+$( "#datepicker" ).datepicker({
+  // Formato de la fecha
+  dateFormat: "dd/mm/yy",
+  // Primer dia de la semana El lunes
+  firstDay: 1,
+  // Dias Largo en castellano
+  dayNames: [ "Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado" ],
+  // Dias cortos en castellano
+  dayNamesMin: [ "Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa" ],
+  // Nombres largos de los meses en castellano
+  monthNames: [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" ],
+  // Nombres de los meses en formato corto 
+  monthNamesShort: [ "Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic" ],
+  // Cuando seleccionamos la fecha esta se pone en el campo Input 
+  onSelect: function(dateText) { 
+        $('#fecha').val(dateText);
+    }
+});
